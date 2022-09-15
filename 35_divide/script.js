de21adbb4d67f0dae659d40e6164900a29f30277
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-09-15 18:11:16 +0900

    35_divide

--- 35_divide/script.js
+++ 35_divide/script.js
@@ -6,7 +6,11 @@ const moveToLeft = document.querySelector('#move-to-left');
 
 updateChildrenCount();
 
-save.addEventListener('click', (e) => {
+save.addEventListener('click', () => {
+  // 毎回消しているつもりでも、2回目以降のanimationが効かない
+  // https://stackoverflow.com/questions/36187393/css-animation-wont-apply-for-the-second-time
+  clearAlert();
+
   for (const child of group1.children) {
     console.log(child.textContent.trim());
   }
@@ -14,6 +18,24 @@ save.addEventListener('click', (e) => {
   for (const child of group2.children) {
     console.log(child.textContent.trim());
   }
+
+  // 【解決】
+  // 毎回消しているつもりでも、2回目以降のanimationが効かない
+  // clearAlert でやっているdomのクリアが非同期っぽく、setTimeoutで後回しになれば、domのクリアを待ってから処理できる。
+  // 結果としてanimationが効く。
+  setTimeout(() => {
+    if (group1.children.length === 0) {
+      showAlert('Group#1が0件になっています。');
+      return;
+    }
+
+    if (group2.children.length === 0) {
+      showAlert('Group#2が0件になっています。');
+      return;
+    }
+
+    showSuccess('保存しました。');
+  }, 0);
 });
 
 group1.addEventListener('click', (e) => {
@@ -74,8 +96,49 @@ function updateChildrenCount() {
   group1Count.classList.add('bump');
   group2Count.classList.add('bump');
 
+  // 削除しないと2回目以降にbumpしなくなる
   setTimeout(() => {
     group1Count.classList.remove('bump');
     group2Count.classList.remove('bump');
   }, 300);
+}
+
+function showSuccess(message) {
+  const success = document.createElement('div');
+  success.className = 'alert alert-success';
+  success.textContent = message;
+
+  const successContainer = document.querySelector('#success-container');
+  successContainer.className = 'show';
+  successContainer.appendChild(success);
+
+  setTimeout(() => {
+    success.remove();
+
+    // ここでクリアしないと2回目以降、効かなくなる。
+    successContainer.className = '';
+  }, 5000);
+}
+
+function clearAlert() {
+  // 2回目以降のanimationが効かない
+  // https://stackoverflow.com/questions/36187393/css-animation-wont-apply-for-the-second-time
+
+  const alert = document.querySelector('.alert.alert-danger');
+  if (alert) {
+    alert.remove();
+  }
+
+  const alertContainer = document.querySelector('#alert-container');
+  alertContainer.className = '';
+}
+
+function showAlert(message) {
+  const alert = document.createElement('div');
+  alert.className = 'alert alert-danger';
+  alert.textContent = message;
+
+  const alertContainer = document.querySelector('#alert-container');
+  alertContainer.className = 'show';
+  alertContainer.appendChild(alert);
 }
