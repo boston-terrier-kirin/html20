3f60841d2bb3ed6fa532425efaf9f4cae21904e6
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-13 18:25:12 +0900

    77_product-card

---
+++ 77_product-card/script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const color1 = document.querySelector('.color-1');
+const color2 = document.querySelector('.color-2');
+
+color1.addEventListener('click', () => {
+  container.classList.remove('change');
+});
+
+color2.addEventListener('click', () => {
+  container.classList.add('change');
+});
