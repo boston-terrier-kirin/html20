8dd4e2031986e80e3e080d9156c77c6caea1bc3a
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-15 15:20:49 +0900

    83_hamburger-menu

--- 83_hamburger-menu/script.js
+++ 83_hamburger-menu/script.js
@@ -1,7 +1,9 @@
 const menuIcon = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector('.navbar');
+const overlay = document.querySelector('.overlay');
 
 menuIcon.addEventListener('click', () => {
   menuIcon.classList.toggle('change');
   navbar.classList.toggle('change');
+  overlay.classList.toggle('change');
 });
