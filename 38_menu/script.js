01de9caf5d92c202af32ce09024d2088ab92db41
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-12-15 18:32:53 +0900

    menu

--- 38_menu/script.js
+++ 38_menu/script.js
@@ -3,5 +3,4 @@ const toggler = document.querySelector('#toggler');
 toggler.addEventListener('click', () => {
   document.querySelector('.left-side-menu').classList.toggle('close');
   document.querySelector('.content-wrapper').classList.toggle('close');
-  document.querySelector('.custom-navbar').classList.toggle('close');
 });
